928c7526c0349844dcfafef0ba81e312ae9e4490
dnye13 <dermotnye@gmail.com>
2024-01-20 21:50:23 -0700

    week2

---
+++ w02-task/scripts/w02-task.js
@@ -0,0 +1,50 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+let fullName = 'Dermot Nye';
+
+let currentYear = new Date().getFullYear();
+
+let profilePicture = 'images/self.jpg';
+
+
+/* Step 3 - Element Variables */
+const nameElement = document.getElementById('name');
+
+const foodElement = document.getElementById('food');
+
+const yearElement = document.querySelector('#year');
+
+const imageElement = document.querySelector('img');
+
+/* Step 4 - Adding Content */
+
+nameElement.innerHTML = '<strong>${fullName}</strong>';
+
+yearElement.textContent = currentYear;
+
+imageElement.setAttribute('src', profilePicture)
+
+imageElement.setAttribute('alt', `Profile image of ${fullName}`);
+
+/* Step 5 - Array */
+
+const favoriteFoods = ["Pizza", "Ice Cream", "Cookies", "Steak"];
+
+foodElement.innerHTML = favoriteFoods.join('<br>');
+
+const anotherFavoriteFood = "Lasagna";
+
+favoriteFoods.push(anotherFavoriteFood);
+
+foodElement.innerHTML += `<br>${favoriteFoods.join('<br>')}`;
+
+favoriteFoods.shift();
+
+foodElement.innerHTML += `<br>${favoriteFoods.join('<br>')}`;
+
+favoriteFoods.pop();
+
+foodElement.innerHTML += `<br>${favoriteFoods.join('<br>')}`;
